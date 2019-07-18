feb0e22df063862ab652cbddc880b4d25f269562
8abc <anadelaine.catalla@gmail.com>
2019-07-17 18:30:20 -0700

    required dependencies and started server

---
+++ server.js
@@ -0,0 +1,41 @@
+// require dependencies
+const express = require('express');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+
+// scraping tools
+const cheerio = require('cheerio');
+const axios = require('axios');
+
+// require all models
+const db = require('./models');
+const PORT = 3002;
+
+// initializes express
+const app = express();
+
+// use morgan logger for logging request
+app.use(logger('dev'));
+// parse request body as json
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+// makes public a static folder
+app.use(express.static('public'));
+
+console.log('\n------------------' + 
+    '\nGraddbing every thread name and link' +
+    'from highsnobiety: ' + 
+    '\n------------------');
+
+// makes a require via axios for highsnobiety
+axios.get('https://www.highsnobiety.com/').then(function(response){
+    // loads and saves html into cheerio
+    const $ = cheerio.load(response.data);
+    // saves the data that we'll scrape
+    let results = [];
+
+});
+// starts the server
+app.listen(PORT, () => {
+    console.log(`App running on port: ${PORT}`);
+});
