bf1c34379f008237ee0515c43a683564c76512ed
8abc <anadelaine.catalla@gmail.com>
2019-07-18 11:43:07 -0700

    created article model and exported moddels in index

---
+++ models/article.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+// saves a reference to the schema constructor
+const Schema = mongoose.Schema;
+
+const ArticlSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    link: {
+        type: String,
+        required: true
+    },
+    // note is an object that stores a note ID
+    // the ref property links the objectID to the note model 
+    // allows us to populate the Article with an associated Note
+    note: {
+        type: Schema.Types.ObjectId,
+        ref: 'Note'
+    }
+});
+
+// creates model from the schema above, using mongoose's model method
+const Article = mongoose.model('Article', ArticlSchema);
+
+module.exports = Article;
--- server.js
+++ server.js
@@ -22,19 +22,27 @@ app.use(express.json());
 // makes public a static folder
 app.use(express.static('public'));
 
+// conncet to the mongo db
+// mongoose.connect('mongodb://localhost/scraperdb', {})
+
 console.log('\n------------------' + 
     '\nGraddbing every thread name and link' +
-    'from highsnobiety: ' + 
+    '\nfrom highsnobiety: ' + 
     '\n------------------');
 
-// makes a require via axios for highsnobiety
-axios.get('https://www.highsnobiety.com/').then(function(response){
-    // loads and saves html into cheerio
-    const $ = cheerio.load(response.data);
-    // saves the data that we'll scrape
-    let results = [];
+// Routes
+// A GET route for scraping 
+app.get('/scrape',(req,res)=>{
+    // makes a require via axios for highsnobiety
+    axios.get('https://www.highsnobiety.com/').then(function(response){
+        // loads and saves html into cheerio
+        const $ = cheerio.load(response.data);
+        // saves the data that we'll scrape
+        let results = [];
 
+    });
 });
+
 // starts the server
 app.listen(PORT, () => {
     console.log(`App running on port: ${PORT}`);
