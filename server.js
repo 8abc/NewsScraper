07fca463645dbc6b4fb80547b976dd05961ac4bc
8abc <anadelaine.catalla@gmail.com>
2019-07-23 21:27:05 -0700

    fixed partials

--- server.js
+++ server.js
@@ -27,14 +27,12 @@ app.use(express.json());
 
 // sets up handlebars 
 const exphbs = require("express-handlebars"); 
-app.engine("handlebars", exphbs({ defaultLayout: "main", layoutsDir: "views/layouts", partialsDir: [
-  path.join(__dirname, "views/partials")
-] }));
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // connects to mongoose database
 const MONGODB_URI =
-  process.env.MONGODB_URI || "mongodb://localhost/scraper_news";
+  process.env.MONGODB_URI || "mongodb://localhost/scraper";
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 var db = mongoose.connection;
@@ -51,6 +49,7 @@ app.use(logger("dev"));
 app.get("/", (req, res)=>{
   res.render("index");
 });
+ 
 console.log('\n------------------' + 
     '\nGrabbing every thread name and link' +
     '\nfrom hypebae.com: ' + 
@@ -76,8 +75,18 @@ app.get("/scrape", (req,res)=>{
   });
 });
 
-
+app.get("/articles", (req,res)=>{
+  Article.find().sort({_id:-1}).exec(function(err,doc){
+    if(err) {
+      console.log(err);
+    } else {
+      let articles = {article: doc};
+      res.render("index",articles)
+    }
+  });
+});
 // starts the server
 app.listen(PORT, () => {
     console.log(`App running on port: ${PORT}`);
 });
+
